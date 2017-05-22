36f72c3ba9bdbe09472c66593319493d59b3f2d4
Samuel Heavner <samuel.heavner@willowtreeapps.com>
2017-05-22 16:41:10 -0400

    add init command and fix validate to ignore missing local properties

---
+++ ukor-init.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const log = require('./utils/log')
+try {
+  fs.mkdir('./src', () => {
+    log.info('created ./src directory')
+  })
+  fs.mkdir('./src/main', () => {
+    log.info('created ./src/main directory')
+  })
+} catch (e) {
+  log.error(e.message)
+}
+
+let defaultProperties =
+  'buildDir: build\n' +
+  'sourceDir: src\n' +
+  'mainFlavor: main\n'
+project = fs.createWriteStream('./ukor.properties')
+project.write(defaultProperties, () => {
+  project.close()
+  log.info('created ukor.properties')
+})
