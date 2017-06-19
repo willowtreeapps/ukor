d20ea9f92acfe4eebb03e02bd54830c5991d1a85
Samuel Heavner <samuel.heavner@willowtreeapps.com>
2017-06-19 15:17:09 -0400

    added junit output to out/tests/ukorTests.junit/xml

--- commands/test.js
+++ commands/test.js
@@ -1,90 +1,156 @@
-const request = require('request')
-const log = require('../utils/log')
-const installer = require('./install')
-const http = require('http')
-const getIP = require('ip')
+const request = require("request");
+const log = require("../utils/log");
+const installer = require("./install");
+const http = require("http");
+const fs = require("fs");
+const getIP = require("ip");
+const XMLWriter = require("xml-writer");
+const mkdirp = require("mkdirp");
 
 function runLogServer(ip, port, timeout, callback) {
   let logServer = http.createServer((req, res) => {
-    let body = []
-    req.on('data', data => {
-      body.push(data)
-    })
-    req.on('end', () => {
+    let body = [];
+    req.on("data", data => {
+      body.push(data);
+    });
+    req.on("end", () => {
       try {
-        body = Buffer.concat(body).toString()
-        let stats = JSON.parse(body)
-        if(stats) {
-          log.info('------------')
-          log.info('Test Results')
-          log.info('Pass: %d | Fail: %d | Crash: %d', stats.correct, stats.fail, stats.crash)
-          log.info('===================================')
+        body = Buffer.concat(body).toString();
+        let stats = JSON.parse(body);
+        log.pretty("info", "", stats);
+        writeJunit(stats);
+        if (stats) {
+          log.info("------------");
+          log.info("Test Results");
+          log.info(
+            "Pass: %d | Fail: %d | Crash: %d",
+            stats.correct,
+            stats.fail,
+            stats.crash
+          );
+          log.info("===================================");
           stats.suites.forEach(suite => {
-            log.info('%s: %d tests', suite.name, suite.tests.length)
+            log.info("%s: %d tests", suite.name, suite.tests.length);
             suite.tests.forEach(test => {
-              log.info('\t%s: %s', test.name, test.result)
-              if(test.result != "Success") {
-                log.info('\t\tError %d: %s', test.error.code, test.error.message)
+              log.info("\t%s: %s", test.name, test.result);
+              if (test.result != "Success") {
+                log.info(
+                  "\t\tError %d: %s",
+                  test.error.code,
+                  test.error.message
+                );
               }
-            })
-            log.info('---------------------------------')
-          })
-          log.info('Pass: %d | Fail: %d | Crash: %d', stats.correct, stats.fail, stats.crash)
+            });
+            log.info("---------------------------------");
+          });
+          log.info(
+            "Pass: %d | Fail: %d | Crash: %d",
+            stats.correct,
+            stats.fail,
+            stats.crash
+          );
         }
-        callback ? callback(stats) : null
-        res.statusCode = '200'
+        callback ? callback(stats) : null;
+        res.statusCode = "200";
       } catch (e) {
-        log.error(e.message)
-        res.statusCode = '403'
+        log.error(e.message);
+        res.statusCode = "403";
       }
-      res.end('ok', () => {
-        logServer.close()
-      })
-    })
-  })
+      res.end("ok", () => {
+        logServer.close();
+      });
+    });
+  });
   logServer.listen(parseInt(port), getIP.address(), () => {
-    log.info('listening for tests on %s:%s', getIP.address(), port)
-  })
-  return logServer
+    log.info("listening for tests on %s:%s", getIP.address(), port);
+  });
+  return logServer;
+}
+
+function writeJunit(stats) {
+  const xml = new XMLWriter();
+  xml.startDocument();
+  xml
+    .startElement("testsuites")
+    .writeAttribute("name", "rokuTests")
+    .writeAttribute("tests", "" + stats.total)
+    .writeAttribute("failures", "" + stats.fail);
+  stats.suites.forEach(suite => {
+    xml
+      .startElement("testsuite")
+      .writeAttribute("name", suite.name)
+      .writeAttribute("tests", "" + suite.total)
+      .writeAttribute("failures", suite.fail);
+    suite.tests.forEach(test => {
+      xml
+        .startElement("testcase")
+        .writeAttribute("name", test.name)
+        .writeAttribute("time", "" + test.time * 0.001);
+      if (test.result != "Success") {
+        xml
+          .startElement("failure")
+          .writeAttribute("type", "" + test.error.code)
+          .text(test.error.message)
+          .endElement();
+      }
+      xml.endElement();
+    });
+    xml.endElement();
+  });
+  xml.endElement();
+  xml.endDocument();
+  mkdirp("./out/tests");
+  fs.writeFileSync("./out/tests/ukorTests.junit", xml.toString());
+  fs.writeFileSync("./out/tests/ukorTests.xml", xml.toString());
 }
 
 function runTests(ip, auth, port, callback) {
   setTimeout(() => {
-    let url = 'http://' + ip + ':8060/launch/dev?RunTests=true&host=' + getIP.address() + '&port=' + port
-    log.verbose('starting tests with: %s', url)
-    request.post(url, {
+    let url =
+      "http://" +
+      ip +
+      ":8060/launch/dev?RunTests=true&host=" +
+      getIP.address() +
+      "&port=" +
+      port;
+    log.verbose("starting tests with: %s", url);
+    request.post(
+      url,
+      {
         auth: auth
-      },(err, response, body) => {
-      if (response.statusCode != 200) {
-        log.error('Error starting tests: %d', response.statusCode);
-        process.exit(-1)
-      } else {
-        log.info('Tests started')
+      },
+      (err, response, body) => {
+        if (response.statusCode != 200) {
+          log.error("Error starting tests: %d", response.statusCode);
+          process.exit(-1);
+        } else {
+          log.info("Tests started");
+        }
+        callback ? callback(response.statusCode == 200) : null;
       }
-      callback ? callback(response.statusCode == 200) : null
-    })
-  }, 3000)
+    );
+  }, 3000);
 }
 
 module.exports = {
   test: (options, callback) => {
     installer.installTest(options, (ip, success) => {
-      log.info('finished install, running tests')
-      if (!success) return
-      let port = options.port || '8086'
-      let server = runLogServer(ip, port, 5 * 60 * 1000, (results) => {
-        if(results){
-          process.exit(0)
-        }else{
-          process.exit(-1)
+      log.info("finished install, running tests");
+      if (!success) return;
+      let port = options.port || "8086";
+      let server = runLogServer(ip, port, 5 * 60 * 1000, results => {
+        if (results) {
+          process.exit(0);
+        } else {
+          process.exit(-1);
         }
-      })
-      runTests(ip, options.auth, options.port, (success) => {
+      });
+      runTests(ip, options.auth, options.port, success => {
         if (!success) {
-          logServer.close()
-          process.exit(-1)
+          logServer.close();
+          process.exit(-1);
         }
-      })
-    })
+      });
+    });
   }
-}
+};
