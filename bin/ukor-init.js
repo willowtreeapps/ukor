1a382f2f93b4ecbbec1d52468ee57ff12d63a34e
Samuel Heavner <samuel.heavner@willowtreeapps.com>
2018-05-03 14:05:50 -0400

    fix init, make a better project structure

--- bin/ukor-init.js
+++ bin/ukor-init.js
@@ -1,41 +1,31 @@
 'use strict'
 const path = require('path')
-const fs = require('fs')
+const fs = require('fs-extra')
 const log = require('../lib/utils/log')
 const yaml = require('js-yaml')
+
 try {
-  fs.mkdir('./src', (err) => {
-    if (err && err.code !== 'EEXIST') throw err
-    log.info('created ./src directory')
+  fs.copySync(path.resolve(__dirname, '../lib/sample'), './', {
+    overwrite: false, 
+    errorOnExist: false
   })
-  fs.mkdir('./src/main', (err) => {
-    if (err && err.code !== 'EEXIST') throw err
-    log.info('created ./src/main directory')
-  })
-  fs.mkdir('./src/test', (err) => {
-    if (err && err.code !== 'EEXIST') throw err
-    log.info('created ./src/test directory')
-  })
-  fs.mkdir('./src/test/source', (err) => {
-    if (err && err.code !== 'EEXIST') throw err
-    log.info('created ./src/test/source directory')
-  })
-  fs.copyFile(path.resolve(__dirname, '../lib/raw/UnitTestFramework.brs'), './src/test/source/UnitTestFramework.brs', (err) => {
-    if (err && err.code !== 'EEXIST') throw err
-    log.info('copied UnitTestFramework.brs to ./src/test/source/')
-  })
-  fs.copyFile(path.resolve(__dirname, '../lib/raw/default.properties'), './src/test/source/ukor.properties', (err) => {
-    if (err && err.code !== 'EEXIST') throw err
-    log.info('copy default properties to ukor.properties')
-  })
+  log.info('created src directory')
+} catch (e) {
+  log.error('failed to create src directory')
+  log.error(e)
+}
   
-  const project = fs.createWriteStream('./ukor.properties.yaml')	
+const propsFile = './ukor.properties.yaml'
+if (!fs.existsSync(propsFile)) {
+  const project = fs.createWriteStream(propsFile)	
   const defaults = {
     name: path.basename(process.cwd()), 
     version: '0.0.1',
     buildDir: 'build',
     sourceDir: 'src',
-    mainFlavor: 'main', 
+    defaults: {
+      falvor: 'main'
+    }, 
     flavors: {
       main: {
         src: ['main']
@@ -44,10 +34,8 @@ try {
   }
   project.write(yaml.safeDump(defaults), () => {	
     project.close()	
-    log.info('created ukor.properties.yaml')	
- })
-
-} catch (e) {
-  log.error(e.message)
+    log.info(`created ${propsFile}`)	
+  })
+} else {
+  log.info(`${propsFile} already exists`)
 }
-
