d1aee5858964c499647f77c5b47f4ead18c42a8a
Cazacu Tudor <cazacu.tudor97@gmail.com>
2019-07-30 17:30:02 +0300

    [Issue 64] ukor-init.js - use ukor.properties.yaml from sample app

--- bin/ukor-init.js
+++ bin/ukor-init.js
@@ -6,7 +6,7 @@ const yaml = require('js-yaml')
 
 try {
   fs.copySync(path.resolve(__dirname, '../lib/sample'), './', {
-    overwrite: false, 
+    overwrite: false,
     errorOnExist: false
   })
   log.info('created src directory')
@@ -14,31 +14,5 @@ try {
   log.error('failed to create src directory')
   log.error(e)
 }
-  
-const propsFile = './ukor.properties.yaml'
-if (!fs.existsSync(propsFile)) {
-  const project = fs.createWriteStream(propsFile)	
-  const defaults = {
-    name: path.basename(process.cwd()), 
-    version: '0.0.1',
-    buildDir: 'build',
-    sourceDir: 'src',
-    defaults: {
-      flavor: 'main'
-    }, 
-    flavors: {
-      main: {
-        src: ['main']
-      }
-    } 
-  }
-  project.write(yaml.safeDump(defaults), () => {	
-    project.close()	
-    log.info(`created ${propsFile}`)	
-  })
-} else {
-  log.info(`${propsFile} already exists`)
-}
 
 log.info('Ukor works greate with Wist, run "wist -i" to generate a default .wistrc.json')
-
